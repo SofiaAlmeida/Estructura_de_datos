6339bdee93dd33d5ccbeb0382143872d73596ee9
pedrobn23 <pedrobonillanadal@hotmail.com>
2016-10-26 17:26:42 +0200

    Pedro

---
+++ Practica2/cronologia/include/chronologyP.h
@@ -0,0 +1,48 @@
+// NOTE yo tampoco sé donde poner lo de autores si justo aquí o en la descripción de la clase
+/**
+  * @file historic_event.h
+  * @brief Fichero cabecera del TDA HistoricEvent
+  *
+  */
+
+#ifndef __CRONOLOGY
+#define __CRONOLOGY
+
+#include "historic_event.h"
+
+/**
+  *  @brief T.D.A. Cronology
+  *
+  * Una instancia @e h del tipo de datos abstracto @c Cronology se compone
+  * de un conjunto de eventos históricos. Se representara un vector de eventos
+  * históricos. Ver documentación de historic_event para más información.
+  *
+  	TODO (si sobra tiempo)
+  * Un ejemplo de su uso:
+  * @include
+  */
+
+class Cronology {
+
+ private: // TODO
+/**
+  * @page repConjunto Rep del TDA Cronology
+  *
+  * @section invConjunto Invariante de la representación
+  *
+  * El invariante es \e
+  *
+  * @section faConjunto Función de abstracción
+  *
+  * Un objeto válido @e rep del TDA Cronology representa
+  *
+  *
+  *
+*/
+	vector<Cronology> befalls; /**< vector con los eventos historicos */
+
+ public:
+
+};
+
+#endif
