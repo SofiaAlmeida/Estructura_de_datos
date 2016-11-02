f9248da90776724be86b147a10e98d4c5f6d9d8e
SofíaAlmeida <sofiil97@gmail.com>
2016-11-02 18:04:54 +0100

    Renombrados los archivos para que concuerden con Pila_max.h

---
+++ Practica3/include/pila_max_cola.h
@@ -0,0 +1,24 @@
+/***
+   * @file Pila_max_Cola.h
+   * @brief Fichero cabecera del TDA Pila_max_Cola
+   * Pila ordenada implementada a partir de cola.h
+   *
+   * @author Sofía Almeida Bruno (@SofiaAlmeida)
+   * @author Jesús Sánchez de Lechina Tejada (@jojelupipa)
+   * @author Pedro Bonilla Nadal (@pedrobn23)
+   */
+
+#ifndef __PILA_MAX_COLA_H__
+#define __PILA_MAX_COLA_H__
+
+#include <cassert>
+
+/***
+   *  @brief T.D.A. Pila_max_Cola
+   *
+   * La descripción de este T.D.A coincide con la del T.D.A @pila_max.h
+   * Este archivo contiene el módulo realizado a partir de colas
+   */
+
+template <class T>
+class Pila_max
