c393c7a4136a2ab5aae8007a946dec9d4a2c1eae
jojelupipa <jesus_38b@hotmail.com>
2016-12-09 18:26:59 +0100

    Añadidas funciones extra a Chronology

--- Practica4/cronologia_stl/cronologia_stl/src/chronology.cpp
+++ Practica4/cronologia_stl/cronologia_stl/src/chronology.cpp
@@ -20,7 +20,7 @@ set<string> Chronology::get_befalls(int date) {
 }
 
 // Insertar acontecimiento
-void Chronology::InsertBefall(int date, const string &s) {
+void Chronology::insert_befall(int date, const string &s) {
   Chronology::iterator it = events.find(date);
 
   //Si la fecha ya está, añadimos el string 
@@ -34,7 +34,7 @@ void Chronology::InsertBefall(int date, const string &s) {
 }
 
 // Insertar evento
-bool Chronology::InsertEvent(const HistoricEvent &h) {
+bool Chronology::insert_event(const HistoricEvent &h) {
   Chronology::iterator it = events.find(h.first);
 
   if(it == events.end()) {
@@ -109,6 +109,142 @@ map<int, HistoricEvent> Chronology::prev_events(int d) {
 	
    return result;
 }
+
+ // FUNCIONALIDADES EXTRA
+
+ // Unión cronologías
+void concat_chron(string input_1, string input_2, string output) {
+
+  Chronology chron1, chron2, chron3;
+
+  ifstream file1, file2;
+  
+  if (file1.open(input_1)) {				 
+    file1 >> chron1;	
+    file1.close();
+  }
+  else
+    cout << "No se pudo leer el archivo " << input_1 << endl;
+  
+
+  if (file2.open(input_2)) {
+    file2 >> chron2;
+    file2.close();
+  }
+  else
+    cout << "No se pudo leer el archivo " << input_2 << endl;
+
+  chron3 = chron1 + chron2; 
+
+  ofstream file3;
+  
+  if (file3.open(output)) {
+    file3 << chron3;
+    file3.close();
+  }
+  
+  else
+    cout << "No se pudo abrir el archivo " << output << endl;
+  
+  
+  return 0;
+}
+
+// Filtro por palabras
+Chronology::Chronology word_filter(const string& word) {
+
+  Chronology chron;
+  istream input;
+
+  if (input.open(word)) {
+    input >> chron;
+    input.close();
+  }
+  else
+    cout << "Error en la apertura del fichero " << word << endl;
+
+  chron = word_search(word);
+  
+  return chron;
+}
+
+
+// Filtro por fecha versión salida fichero
+void date_filter(const string& filein, const int& lower, const int& upper, const string& fileout) {
+
+  Chronology chron;
+  istream in;
+
+  if (in.open(word)) {
+    in >> chron;
+    in.close();
+  }
+  else
+    cout << "Error en la apertura del fichero " << filein << endl;
+
+  chron = range(lower, upper);
+
+  ofstream out;
+  
+  if (out.open(fileout)) {
+    out << chron;
+    out.close();
+  }
+  else
+    cout << "Error en la apertura del fichero " << fileout << endl;
+}
+
+
+// Filtro por fecha versión salida estándar
+void date_filter(const string& filein, const int& lower, const int& upper) { 
+ 
+  Chronology chron;
+  istream in;
+
+  if (in.open(word)) {
+    in >> chron;
+    in.close();
+  }
+  else
+    cout << "Error en la apertura del fichero " << filein << endl;
+
+  chron = range(lower, upper);
+  
+  cout << chron;
+}
+
+// Estadísticas 
+void stats(const string& filename){
+
+  istream in;
+  Chronology chron;
+
+  if (in.open(filename)){
+    in >> chron;
+    in.close();
+  }
+  else
+    cout << "Error en la apertura de " << filename << endl;
+
+  double total_years = 0;
+  double total_befalls = 0, mean_befalls; // Total de eventos y promedio de eventos/año
+
+  Chronology::const_iterator c_it;
+
+  total_years = events.size();
+  
+  for(c_it = begin(); c_it != end(); ++it) 
+    total_befalls += (*it).second.befalls_size();
+  
+  mean_befalls = total_befalls / total_years;
+  
+  cout << "Hay un total de " << total_befalls << " ocurridos en " << total_years << " años\nEsto hace una media de " << mean_befalls << " eventos/año" << endl;
+  
+}
+
+
+
+ 
  //Sobrecarga operador +
 Chronology& Chronology::operator+(const Chronology &c) {
   Chronology::iterator it;
